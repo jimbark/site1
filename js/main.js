7731de6a587bb1333bac42aaebf837a17e159e22
Jim Bark <jimbark007@gmail.com>
2014-07-14 20:40:01 +0100

    first complete bootstrap layout for birthdays

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$('#next').click(function () {
+    // do something
+    $('#name').css('background', 'red');
+});
+
+$('#prev').click(function () {
+    // do something
+    $('#name').css('background', 'none');
+});
+
 
